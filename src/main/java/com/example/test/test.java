d0a7bda79fca83952d3cbddf8b59063b0909bb09
NNShadow <1581333251@qq.com>
2021-01-06 10:19:06 +0800

    add test

---
+++ src/main/java/com/example/test/test.java
@@ -0,0 +1,11 @@
+package com.example.test;
+
+/**
+ * @author NNroc
+ * @date 2021/1/6 10:17
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("tt");
+    }
+}
